8016d4f12489dc52581eb9977d0dc2fad2ccfcac
Audrey <aeryan@uci.edu>
2018-05-09 03:06:58 -0700

    Testing new routing system

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom'; 
+import React from 'react';
 import './App.css';
 
-/* Import pages*/
-import Home from './pages/Home.jsx';
-import Upload from './pages/Upload.jsx';
-import ManageUploads from './pages/ManageUploads.jsx';
-import CreateAnalysis from './pages/CreateAnalysis.jsx';
-import Results from './pages/Results.jsx';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-        <Router>
-            <div class="wrapper">
-                <nav id="sidebar-wrapper">
-                    <ul class="sidebar-nav">
-                        <a href="/">Home</a>
-                        <a href="/create/analysis">Start New Analysis</a>
-                        <a href="/results">Saved Results</a>
-                        <a href="/upload">Upload Data Sets</a>
-                        <a href="/upload/manage">Manage Data Sets</a>
-                    </ul>
-                </nav>
 
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/create/analysis" component={CreateAnalysis}/>
-                <Route exact path="/results" component={Results}/>
-                <Route exact path="/upload" component={Upload}/>
-                <Route exact path="/upload/manage" component={ManageUploads}/>
-            </div>
-        </Router>
-        
-    );
-  }
-}
+const App = () => (
+    <div className="app">
+        <h1>React Router Demo</h1>
+        <Navigation />
+        <Main />
+    </div>
+);
+
+const Navigation = () => (
+    <nav>
+        <ul>
+            <li><NavLink to ="/">Home</NavLink></li>
+            <li><NavLink to ="/test">Test</NavLink></li>
+        </ul>
+    </nav>
+);
+
+const Home = () => (
+    <div className="home">
+        homebody
+    </div>
+);
+
+const Test = () => (
+    <div className="test">
+        body text
+    </div>
+);
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/test" component={Test}></Route>
+    </Switch>
+)
 
 export default App;
