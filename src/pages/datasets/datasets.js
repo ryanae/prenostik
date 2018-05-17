f51d57bf236c597b7806be2f03e3783e75cff7b8
Karen <karen.liang95@gmail.com>
2018-05-17 16:28:10 -0700

    adding css to saved results page

--- src/pages/datasets/datasets.js
+++ src/pages/datasets/datasets.js
@@ -5,7 +5,16 @@ import React from 'react';
 const datasets = () => (
     <div className="datasets">
         datasheets page
+    
+    <input type="file" id="excelfile" />  
+    <input type="button" id="viewfile" value="Export To Table" onclick="ExportToTable()" />  
+      <br />  
+      <br />  
+    <table id="exceltable">  
+    </table> 
     </div>
 );
 
 export default datasets;
+
+function
--- src/pages/saved/savedResults.js
+++ src/pages/saved/savedResults.js
@@ -1,6 +1,7 @@
 /* This is the main/first page for Saved Results */
 
 import React from 'react';
+import './savedResults.css';
 
 const savedResults = () => (
     
