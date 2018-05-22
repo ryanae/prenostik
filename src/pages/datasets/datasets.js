6116d9e5dfc5c7df73647a0e4e8195eb009bd8bd
Karen <karen.liang95@gmail.com>
2018-05-22 16:15:20 -0700

    updating data upload and removing table button

--- src/pages/datasets/datasets.js
+++ src/pages/datasets/datasets.js
@@ -1,20 +1,43 @@
 /* This is the main/first page for view/manage/upload datasets. */
 
 import React from 'react';
+import './dataset.css';
+import {handleFiles} from './csv_file.js';
+import {read_file} from './csv_file.js'; 
+import {window_test} from './csv_file.js';
+import CsvParse from '@vtex/react-csv-parse'; 
+
+
 
 const datasets = () => (
     <div className="datasets">
         datasheets page
     
-    <input type="file" id="excelfile" />  
-    <input type="button" id="viewfile" value="Export To Table" onclick="ExportToTable()" />  
+    <p> Choose your file: </p>
+    <input id="csv" type="file"></input>
+
+    <input type="button" id="csvSubmit" value="Submit" onclick={()=>window_test()}
+    accept=".csv"/> 
+    
+    
+    <output id="out">
+        file contents here 
+    </output>
+
+    
+    
+    <input type="file" id="csvFileInput" onChange={handleFiles(this.files)} accept=".csv"/>  
+    <output id="output">
+        output test 2
+    </output>
+    
       <br />  
       <br />  
-    <table id="exceltable">  
-    </table> 
+    
     </div>
 );
 
+
 export default datasets;
 
-function
+
