a8681faf4ec9e5e6fb8787bb3bd5c5ec39c747ca
Karen <karen.liang95@gmail.com>
2018-05-31 17:07:36 -0700

    Updated new analysis and result

--- src/pages/analysis/newAnalysis.js
+++ src/pages/analysis/newAnalysis.js
@@ -1,12 +1,238 @@
-/* This is the main/first page for Create Analysis */
+import React, { Component } from 'react';
+import {Steps, Button, message, Modal, } from 'antd';
+import Calendar from 'react-calendar';
+import ReactDOM from 'react-dom';
+import Select from 'react-select';
+import moment from 'moment';
+import 'antd/dist/antd.css';
+import 'react-select/dist/react-select.css'
+import './newAnalysis.css'; 
 
-import React from 'react';
+const Step = Steps.Step;
 
-const newAnalysis = () => (
-    <div className="newAnalysis">
-        <h2>Start New Analysis</h2>
+class newAnalysis extends Component {
+	constructor (props) {
+		super (props);
+
+
+
+		this.state = {
+			current : 0, visible : false, 
+            lines:[]
+		};
+        
+        //For opening and parsing files
+        this.handleFiles = this.handleFiles.bind(this);
+        this.fileReadingFinished = this.fileReadingFinished.bind(this); 
+	}
+
+  next() {
+    const current = this.state.current + 1;
+    this.setState({ current });
+  }
+
+  prev() {
+    const current = this.state.current - 1;
+    this.setState({ current });
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  showModalTest = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+  
+  //Handles OK click on Modal 
+  handleOk = (e) => {
+    this.setState({
+      visible: false,
+    });
+      
+    //Display list of files that were uploaded
+    var file_read = document.querySelector("input").files[0];   
+    document.getElementById("file_read").innerHTML=file_read.name; 
+  }
+
+  //Handles Cancel click on Modal
+  handleCancel = (e) => {
+    this.setState({ 
+      visible: false,
+    });
+  } 
+  
+  
+  //Handles setting data
+  handleData = data => { 
+        this.setState({data})
+    };
     
-    </div>
-);
 
+    //Handles opening file
+    handleFiles = (files) => {
+        // Check for the various File API support.
+        if (window.FileReader) {
+            // FileReader are supported.
+            console.log("read"); 
+            this.getAsText(files[0]);
+        }
+    };
+
+
+    //Opens file and reads as string
+    getAsText(fileToRead) {
+        var reader = new FileReader();
+        var file_read = document.querySelector("input").files[0];
+        
+        // Read file into memory as UTF-8      
+        reader.readAsText(file_read);
+        // Handle errors load
+        reader.onload = this.fileReadingFinished;
+        reader.onerror = this.errorHandler;
+    };
+
+    //Processes string from file into JSON object
+    fileReadingFinished(event) {
+        var csv = event.target.result;
+        var text = csv + " "; 
+        var allTextLines = text.split(/\r\n|\n/);
+        var lines = allTextLines.map(data => data.split(';'));
+        
+        document.getElementById("output").innerHTML=""; 
+        document.getElementById("output").innerHTML=text; 
+
+        var CSV = require('csv-string'); 
+        //var arr = CSV.parse(lines); 
+
+
+        var s_line = csv.split("\n"); 
+        document.getElementById("output").innerHTML=s_line[0]; 
+
+        var result = []; 
+        var headers = s_line[0].split(",");
+        document.getElementById("output").innerHTML=headers[0]; 
+
+        for (var i = 1; i<s_line.length; i++){ 
+            var json_obj = {};
+            var curr_line = s_line[i].split(","); 
+            for(var j=0; j<headers.length; j++){ 
+                json_obj[headers[j]] = curr_line[j];
+                document.getElementById("output").innerHTML=curr_line[j]; 
+            }
+            result.push(json_obj);
+        }
+
+        document.getElementById("output").innerHTML=JSON.stringify(result);
+
+        //stores the lines into local storage 'lines' so the results page can retrieve it
+        
+        localStorage.setItem('lines', JSON.stringify(result)); 
+        console.log(localStorage.getItem('lines')); 
+
+        this.setState({lines: lines})
+    };
+
+    errorHandler(event) {
+        if (event.target.error.name === "NotReadableError") {
+            alert("Cannot read file!");
+        }
+    };
+
+    //Go to Result page when completed
+    result_analysis(event){ 
+        message.success('Processing complete!');
+        //event.preventDefault(); 
+        window.location = 'result';
+    }
+
+
+  render () {
+  	const {current} = this.state;
+
+	const firstContent = (
+	      <div>     
+	        <div>
+	          <h3> Reference Dataset </h3> 
+                <p id="file_read">File Read</p>
+	          <button className ="button1" onClick={this.showModal}>
+	          	<strong>Select Reference Data </strong>
+	          </button>
+            <Modal
+              title="Manage Datasets"
+              visible = {this.state.visible}
+              onOk = {this.handleOk}
+              onCancel = {this.handleCancel}
+            >
+
+            <strong>My File</strong>
+        
+        
+            <input type="file" onChange={ this.handleFiles }
+                accept=".csv"/>
+            <div id="output">Output Here</div>
+        
+        
+            </Modal>  
+	        </div>
+
+          <div>
+            <br />
+            <h3> Test Data </h3>
+            <p id="test_file_read>"> Test File Read</p>
+            <button className="button1" onClick={this.showModalTest}> 
+              <strong> Select Test Data </strong>
+            </button> 
+          </div>
+	    
+	      </div>
+	    )
+
+    const steps = [{
+      title: 'First',
+      content: firstContent,
+    }, {
+      title: 'Second',
+      content: 'secondContent',
+    }, {
+      title: 'Last',
+      content: 'Last-content',
+    }]; 
+
+
+    return (
+    	<div>
+    		<Steps current={current}>
+	          {steps.map(item => <Step key={item.title} title={item.title} />)}
+	        </Steps>
+	        <div className="steps-content">{steps[this.state.current].content}</div>
+	        <div className="steps-action">
+	          {
+	            this.state.current < steps.length - 1
+	            &&
+	            <Button type="primary" onClick={() => this.next()}>Next</Button>
+	          }
+	          {
+	            this.state.current === steps.length - 1
+	            &&
+	            <Button type="primary" onClick={() => this.result_analysis()}>Done</Button>
+	          }
+	          {
+	            this.state.current > 0
+	            &&
+	            <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
+	              Previous
+	            </Button>
+	          }
+	        </div>
+    	</div>
+    	);
+    }
+}  	
+  
+ReactDOM.render(<newAnalysis />, document.getElementById('root'));
 export default newAnalysis;
--- src/pages/analysis/result.js
+++ src/pages/analysis/result.js
@@ -5,6 +5,11 @@ import './result.css';
 import sample from '../home/chart-example.png';
 import {Bar} from 'react-chartjs-2';
 
+//install csvtojson 
+//npm install convert-csv-to-json --save
+
+//install csv string
+//npm install csv-string
 class result extends Component {
     constructor(props) {
         super(props);
@@ -13,17 +18,47 @@ class result extends Component {
                 labels: ['x', 'y', 'z'],
                 datasets: [
                     {
-                        label: 'Population',
+                        label: 'Confidence Index',
                         data: [1, 2, 3]
                     }
                 ],
-            }
+            },
+            lines: []
         }
+        this.display_lines = this.display_lines.bind(this);
     }
+    
+    
+    display_lines(){ 
+        const cached_lines = localStorage.getItem('lines'); 
+        if (cached_lines){ 
+            console.log("not empty"); 
+            this.setState({lines: cached_lines});
+            console.log(cached_lines);
+        }
+        
+        var JSON_obj = JSON.parse(cached_lines); 
+        //document.getElementById("output").innerHTML=""; 
+        //document.getElementById("output").innerHTML=JSON.stringify(JSON_obj[0]["Date"]); 
+        
+        var Dates = []; 
+        var Confidence_Index = [];
+        for (var i=0; i<JSON_obj.length; i++){
+            Dates.push(JSON_obj[i]["Date"]); 
+            Confidence_Index.push(JSON_obj[i]["Consumer Confidence Index"])
+        }
+        
+        this.state.chartData.labels = Dates; 
+        this.state.chartData.datasets[0].data = Confidence_Index;
+        
+        return;
+    }
+    
+
     
     render() {
         return (
-            <div className="result">
+            <div className="result" onLoad={this.display_lines} >
                 <div class="row">
                     <div class="col-7"><h2><b>Results For</b> Untitled Analysis</h2></div>
 
@@ -43,7 +78,9 @@ class result extends Component {
             
                                 <Bar data={this.state.chartData} />
             
-                                <p class="h6 m-3">Lorem ipsum <button type="button" class="btn btn-light float-right">See More</button> </p>
+                                <p class="h6 m-3" id="output"> Best Predictors 
+                                    <button type="button" class="btn btn-light float-right">See More</button> 
+                                </p>
                             </div>
                         </div>
                     </div>
