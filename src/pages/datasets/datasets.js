0a5e9b65a760b86ee8cbb5e6935af2f8c4606cd2
YuNGPiGgy <yungchu1219@gmail.com>
2018-06-11 09:51:42 -0700

    datasets some changes

--- src/pages/datasets/datasets.js
+++ src/pages/datasets/datasets.js
@@ -1,90 +1,119 @@
 /* This is the main/first page for view/manage/upload datasets. */
 
-import React, { Component } from 'react';
-import './dataset.css';
-import {handleFiles} from '../../App.js';
-import CsvParse from '@vtex/react-csv-parse'; 
+
+import React from 'react';
+
+var datasetList = [];
 
 
-class datasets extends Component {
-    constructor(){ 
-        super(); 
-        this.handleFiles = this.handleFiles.bind(this);
-        //this.processData = this.processData(this); 
-    };
- 
-    handleFiles = (files) => {
-        // Check for the various File API support.
-        if (window.FileReader) {
-            // FileReader are supported.
-            console.log("read"); 
-            this.getAsText(files[0]);
-        }
-    };
-
-    getAsText(fileToRead) {
-        var reader = new FileReader();
-        var file_read = document.querySelector("input").files[0];
+class datasets extends React.Component {
+	constructor(props) {
+		super();
+		
+		// This contains the info about the dataset, in name and ctime attributes.
+		this.datasetDetailsList = [];
+		
+		// This contains the path of each dataset.
+		this.datasetPathList = [];
+                        		
+		super(props);
+		this.getFiles();
+		this.state = { 
+            pathlist : []
+        };
         
-        // Read file into memory as UTF-8      
-        reader.readAsText(file_read);
-        // Handle errors load
-        reader.onload = this.fileReadingFinished;
-        reader.onerror = this.errorHandler;
-    };
+		this.handleUploadImage = this.handleUploadImage.bind(this);
+		this.getFiles = this.getFiles.bind(this);
+	}
+    
+//    checkCSV(file) {
+//        return String(file).endsWith('.csv');
+//    }
+	
+	getFiles() {
+		fetch('http://localhost:8001/getFiles', {
+			method: 'POST',
+		}).then((response) => {
+			response.json().then((body) => {
+				this.datasetDetailsList = body.file;
+				                
+				this.datasetPathList = [];
+				for (var i in this.datasetDetailsList) {
+					this.datasetPathList.push("http://localhost:8001/public/"+this.datasetDetailsList[i]);
+				}
+                localStorage.setItem("pathlist", JSON.stringify(this.datasetDetailsList));
+				this.forceUpdate();
+			});
 
+		});
 
-    fileReadingFinished(event) {
-        var csv = event.target.result;
-        var text = csv + " "; 
-        var allTextLines = text.split(/\r\n|\n/);
-        var lines = allTextLines.map(data => data.split(';'));
+	}
+	
+	handleUploadImage(ev) {
+		ev.preventDefault();
+
+		const data2 = new FormData();
+		data2.append('file', this.uploadInput.files[0]);
+		data2.append('filename', this.fileName.value);
+	
+        if (this.uploadInput.files[0] == null) {
+			return;
+		}
+		
+		if (this.fileName.value == '') {
+			return;
+		}
         
-    document.getElementById("output").innerHTML=""; 
-    document.getElementById("output").innerHTML=lines; 
+		fetch('http://localhost:8001/upload', {
+			method: 'POST',
+			body: data2,
+		}).then((response) => {
+			response.json().then((body) => {
+				this.getFiles();
+			});
+		});
+	}
 
-        
-        //document.write(lines); 
-    };
+    
 
-    errorHandler(event) {
-        if (event.target.error.name === "NotReadableError") {
-            alert("Cannot read file!");
-        }
-    };
-
-    draw (lines){ 
-        document.getElementById("output").innerHTML=""; 
-
-    };
-
-    draw_output(lines){ 
-        console.log("draw"); 
-
-       document.getElementById("output").innerHTML=""; 
-	   var table = document.createElement("table");
-	   for (var i = 0; i < lines.length; i++) {
-		  var row = table.insertRow(-1);
-		  for (var j = 0; j < lines[i].length; j++) {
-			 var firstNameCell = row.insertCell(-1);
-			 firstNameCell.appendChild(document.createTextNode(lines[i][j]));
-		  }
-	   }
-	   document.getElementById("output").appendChild(table)
-    };
-
-    render() {
-        return(
-            <div>
-            <input type="file" onChange={ this.handleFiles }
-                accept=".csv"/>
-            <div id="output">Output Here</div>
-            </div>
-        )
-    }
+	render() {
+		return (
+            <div class="card bg-light mb-3 text-center">
+                <div class="card-body">
+                    <div class="col">
+                        <div class="row">
+                            <h3> Datasets </h3>
+                            
+                            <div>
+                                <input ref={(ref) => { this.fileName = ref; }} type="text" placeholder="Searth through my datasets" />
+                                {this.datasetDetailsList.map((dataset) => {return (<ul><li>{dataset}</li></ul>)})}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <form onSubmit={this.handleUploadImage}>
+                                <div class="card bg-light mb-3 text-center">
+                                    <div class="card-body">
+                                        <div>
+                                            <input ref={(ref) => { this.uploadInput = ref; }} type="file"/>
+                                            
+                                            <div>
+                                                <button class="btn btn-success">Upload</button>
+                                                <h5> or Drag and Drop!</h5>
+                                                <p> .csv files only </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                <div class="col">
+                    here
+                </div>
+        </div>
+    </div>
+    );
+	}
 }
 
-
 export default datasets;
-
-
